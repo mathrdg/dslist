3b1b144f769d5d69329dd7922538698a98391b92
mathrdg <math.rdg@outlook.com>
2025-05-25 12:45:15 -0300

    GET name by id

--- src/main/java/com/mathrss/dslist/controllers/GameController.java
+++ src/main/java/com/mathrss/dslist/controllers/GameController.java
@@ -3,12 +3,14 @@ package com.mathrss.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mathrss.dslist.dto.GameDTO;
 import com.mathrss.dslist.dto.GameMinDTO;
 import com.mathrss.dslist.services.GameService;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 
@@ -20,6 +22,12 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value="/{id}")    
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+    
     @GetMapping    
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findall();
